426de8f5b39fc02b3f00cbd05d1e25742acd1414
chenghuaWang <chenghua.wang.edu@gmail.com>
2026-05-27 05:31:17 +0000

    fix(phyai): duplicate load checkpoint functions

--- phyai/src/phyai/utils/__init__.py
+++ phyai/src/phyai/utils/__init__.py
@@ -1,10 +1,9 @@
-from phyai.utils.checkpoint import find_safetensors, load_checkpoint, load_config
+from phyai.utils.checkpoint import find_safetensors, load_config
 from phyai.utils.logging import all_ranks_log, this_rank_log
 
 __all__ = [
     "all_ranks_log",
     "find_safetensors",
-    "load_checkpoint",
     "load_config",
     "this_rank_log",
 ]
